aa49fb29460fdc87fcac8f22e702f639ed8d0b85
Scott Motte <mot@mot.la>
2023-04-01 10:50:04 -0700

    Change hostname

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv-vault-core').config()
 
 const http = require('http')
 
-const hostname = '127.0.0.1'
+const hostname = '0.0.0.0'
 const port = process.env.PORT
 
 const server = http.createServer((req, res) => {
