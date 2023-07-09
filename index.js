6d15fb4d7de35ebfcf04d3d1bec6475786756ddc
Scott Motte <mot@mot.la>
2023-07-08 22:07:15 -0700

    Basic http

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 // index.js
-require('dotenv-vault-core').config()
-
+const PORT = process.env.PORT || 3000
 const http = require('http')
-
-const hostname = '0.0.0.0'
-const port = process.env.PORT
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
-  res.end(process.env.GREETING)
+  res.end(`Hello ${process.env.HELLO}`)
 })
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+server.listen(PORT, () => {
+  console.log(`Server running on port:${PORT}/`)
 })
